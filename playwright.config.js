95864edfd060a6306c1b6b6196f59007c21bf842
koijamyaiphaba <koijamyaiphaba14@gmail.com>
2025-08-05 02:11:32 +0530

    Add files via upload

---
+++ playwright.config.js
@@ -0,0 +1,125 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './QA_Functionalities/tests',
+  // testDir: './tests',
+  // testDir: '',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['html', { printSteps: true }],
+    ['line', { printSteps: true }],
+    ['allure-playwright', { printSteps: true }]
+  ],
+
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    // baseURL: 'https://the-internet.herokuapp.com/basic_auth',
+    // storageState: 'playwright/.auth/user.json',
+  },
+
+
+  /* Configure projects for major browsers */
+  projects: [
+    // { name: 'setup', testMatch: /.*\.setup\.ts/ },
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        deviceScaleFactor: undefined,
+        viewport: null,
+        // storageState: 'playwright/.auth/user.json',
+        launchOptions: {
+          args: ["--start-maximized"],
+        }
+      },
+      // dependencies: ['setup'],
+    },
+
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      deviceScaleFactor: undefined,
+        viewport: null,
+        launchOptions: {
+          args: ["--start-maximized"],
+        }
+      },
+    },
+
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //     deviceScaleFactor: undefined,
+    //     viewport: null,
+    //     launchOptions: {
+    //       args: ["--start-maximized"],
+    //     }
+    //   },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    {
+      name: 'edge',
+      use: {
+        ...devices['Desktop Edge'], channel: 'msedge',
+        deviceScaleFactor: undefined,
+        viewport: null,
+        launchOptions: {
+          args: ["--start-maximized"],
+        }
+      },
+    },
+
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
